7079623e864734412ab66cf6d6fa9ae6e5c0f89b
ruslanromanenko <ruslanromanenko@gmail.com>
2019-02-25 18:59:11 +0200

    Create Quiz component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Quiz from './Quiz/Quiz';
+
+const dataQuiz = [
+  {question: 'Столица Украины',answer: 'Киев'},
+  {question: 'Столица России',answer: 'Москва'},
+  {question: 'Столица Белорусии',answer: 'Минск'},
+  {question: 'Столица США',answer: 'Вашингтон'}
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Quiz />
       </div>
     );
   }
---
+++ src/Quiz/Quiz.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quiz = (props) => {
+
+  return 'Hello'
+
+};
+
+export default Quiz
