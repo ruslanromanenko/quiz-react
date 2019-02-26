a5ea34fce1c60094a1d2a6a54ef8cc87c5be0cc5
ruslanromanenko <ruslanromanenko@gmail.com>
2019-02-26 12:30:21 +0200

    Save answer User in parent component.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
 import Quiz from './Quiz/Quiz';
+import Button from '@material-ui/core/Button';
+
 
 const dataQuiz = [
-  {question: 'Столица Украины',answer: 'Киев'},
-  {question: 'Столица России',answer: 'Москва'},
-  {question: 'Столица Белорусии',answer: 'Минск'},
-  {question: 'Столица США',answer: 'Вашингтон'}
+  {id:1, question: 'Столица Украины?',answer: 'Киев'},
+  {id:2, question: 'Столица России?',answer: 'Москва'},
+  {id:3, question: 'Столица Белорусии?',answer: 'Минск'},
+  {id:4, question: 'Столица США?',answer: 'Вашингтон'}
 ];
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dataQuiz: dataQuiz,
+      submitted: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({submitted: true});
+  };
+
+  handleChangeInput = (evt, idQuiz) => {
+    let dataQuiz = this.state.dataQuiz;
+    dataQuiz.map(quiz => {
+      if(quiz.id === idQuiz) {
+        quiz.answerUser = evt.target.value;
+      }
+      return quiz;
+    });
+
+    this.setState(dataQuiz);
+  };
+
+
   render() {
     return (
       <div className="App">
-        <Quiz />
+        {
+          this.state.dataQuiz.map(quiz => {
+            return <Quiz
+              quiz={quiz}
+              key={quiz.id}
+              stateButton={this.state.submitted}
+              handleChange={this.handleChangeInput}
+            />
+          })
+        }
+        <Button variant="contained" color="primary" onClick={this.handleClick}>
+          Сдать тест
+        </Button>
       </div>
     );
   }
--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import Input from '@material-ui/core/Input';
+import './Quiz.css';
 
 const Quiz = (props) => {
+  return <div>
+    <span>{props.quiz.question}</span>
+    <br/>
+    {
+      (props.stateButton) ?
 
-  return 'Hello'
+      <div>
+        <span className="answer">{props.quiz.answer}</span>
+        <span className="answerUser">{props.quiz.answerUser}</span>
+      </div> :
+
+      <Input type="text"  onChange={ (evt) => props.handleChange(evt, props.quiz.id)}/>
+    }
+  </div>
 
 };
 
